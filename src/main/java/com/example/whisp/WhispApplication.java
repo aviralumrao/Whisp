354374facce1d46d64b886b6c34635cc858f063b
aviralumrao <aviral0419@gmail.com>
2026-09-19 01:19:58 +0530

    Initial commit

---
+++ src/main/java/com/example/whisp/WhispApplication.java
@@ -0,0 +1,13 @@
+package com.example.whisp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhispApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhispApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/whisp/WhispApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.whisp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhispApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
